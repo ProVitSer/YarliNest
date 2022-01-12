3dae050d40214bf78d220db8dea9e82f8e41a774
ProVitSer <prokinvs@gmail.com>
2022-01-12 14:03:53 +0000

    Взаимодействие с локальной бд

--- src/lowdb/lowdb.module.ts
+++ src/lowdb/lowdb.module.ts
@@ -1,7 +1,11 @@
+import { LoggerModule } from '@app/logger/logger.module';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { LowdbService } from './lowdb.service';
 
 @Module({
-  providers: [LowdbService]
+  imports:[ConfigModule,LoggerModule],
+  providers: [LowdbService],
+  exports: [LowdbService]
 })
 export class LowdbModule {}
