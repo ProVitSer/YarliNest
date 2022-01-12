8b13e81ba5e5d2c8ea62d1a814c686844eaabc87
ProVitSer <prokinvs@gmail.com>
2022-01-12 14:05:32 +0000

    connect module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,11 @@ import { ConfigModule } from '@nestjs/config';
 import { KerioModule } from './kerio/kerio.module';
 import { SyncDataModule } from './sync-data/sync-data.module';
 import { LowdbModule } from './lowdb/lowdb.module';
+import { SeleniumModule } from './selenium/selenium.module';
 import configuration from '@app/config/config.provides';
 
 @Module({
-  imports: [ConfigModule.forRoot({ load: [configuration] }),TGModule, LoggerModule, KerioModule, SyncDataModule, LowdbModule],
+  imports: [ConfigModule.forRoot({ load: [configuration] }),TGModule, LoggerModule, KerioModule, SyncDataModule, LowdbModule, SeleniumModule],
   controllers: [AppController],
   providers: [AppService],
   exports: [ConfigModule]
