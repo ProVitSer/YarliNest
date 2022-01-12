f780129ea91cf00c482b2c657b7a2fb481057c7a
ProVitSer <prokinvs@gmail.com>
2022-01-12 14:06:48 +0000

    add new types

---
+++ src/lowdb/types/interfaces.ts
@@ -0,0 +1,13 @@
+export interface DBConference {
+    id: string,
+    theme: string,
+    organizer: string,
+    fioOrganizer: string,
+    info: string,
+    date: string,
+    hour: string,
+    minute: string,
+    duration: number,
+    emailNumberArray: Array<string>,
+    fio: Array<string>,
+  }
